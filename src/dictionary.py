140176d4b6bdd81985399510cceaca32f545eeda
Nickson Kipruto Rop <nickyrutto@gmail.com>
2022-10-20 16:50:59 +0300

    Dictionaries in Python

---
+++ src/dictionary.py
@@ -0,0 +1,80 @@
+planet = {
+    'name': "Earth",
+    'moons': 1
+}
+
+print(planet['name'])
+
+# name is now set to Makemake
+planet.update({'name': 'Makemake'})
+print(planet)
+
+# Using update
+planet.update({
+    'name': 'Jupiter',
+    'moons': 79
+})
+
+# Using square brackets
+planet['name'] = 'Jupiter'
+planet['moons'] = 79
+
+planet['orbital period'] = 4333
+
+# planet dictionary now contains: {
+#   name: 'jupiter'
+#   moons: 79
+#   orbital period: 4333
+# }
+
+planet.pop('orbital period')
+
+# planet dictionary now contains: {
+#   name: 'jupiter'
+#   moons: 79
+# }
+
+# Add address
+planet['diameter (km)'] = {
+    'polar': 133709,
+    'equatorial': 142984
+}
+
+# planet dictionary now contains: {
+#   name: 'Jupiter'
+#   moons: 79
+#   diameter (km): {
+#      polar: 133709
+#      equatorial: 142984
+#   }
+# }
+print(f'{planet["name"]} polar diameter: {planet["diameter (km)"]["polar"]}')
+
+# Output: Jupiter polar diameter: 133709
+
+rainfall = {
+    'october': 3.5,
+    'november': 4.2,
+    'december': 2.1
+}
+
+for key in rainfall.keys():
+    print(f'{key}: {rainfall[key]}cm')
+
+# Output:
+# october: 3.5cm
+# november: 4.2cm
+# december: 2.1cm
+
+total_rainfall = 0
+for value in rainfall.values():
+    total_rainfall = total_rainfall + value
+
+print(f'There was {total_rainfall}cm in the last quarter')
+
+# Output:
+# There was 10.8cm in the last quarter
+
+
+
+
